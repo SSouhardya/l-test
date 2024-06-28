fd401be07bfa4cc4b14a187073ffcc482b4364b0
Souhardya Sengupta <62637393+SSouhardya@users.noreply.github.com>
2024-06-28 11:00:04 -0400

    Update combine_ltest_variability.R

--- reproducibility/combine_ltest_variability.R
+++ reproducibility/combine_ltest_variability.R
@@ -4,12 +4,12 @@ sd_tab = 0
 m = 200
 for(i in 1:total){
 	temp = (read.csv(paste0('variation_single_test_',i,'.csv'), header = TRUE)[,-1])
-	temp[,1] = sqrt(temp[,1]/(m*(m-1)))
-	temp[,2] = sqrt(temp[,2]/(m^2-1))
+	temp[,1] = 0.5*log(temp[,1]/(m*(m-1)))
+	temp[,2] = 0.5*log(temp[,2]/(m^2-1))
 	mean_tab = mean_tab + temp
 	sd_tab = sd_tab + temp^2
 }
 mean_tab = mean_tab/total
 sd_tab = sqrt(sd_tab/total - mean_tab^2)/sqrt(total)
 ll = list(mean_tab, sd_tab)
-saveRDS(ll, 'variation_sigle_test_non_log_list.list')
+saveRDS(ll, 'variation_ltest_log.list')
