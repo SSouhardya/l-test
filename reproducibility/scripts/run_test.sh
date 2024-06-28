52422215d42a2d50778fb61e2c574ee16abd2839
Souhardya Sengupta <62637393+SSouhardya@users.noreply.github.com>
2024-06-28 05:17:06 -0400

    Update and rename run_simulations.sh to run_test.sh

--- reproducibility/scripts/run_test.sh
+++ reproducibility/scripts/run_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH -J  # Add a job name for the array
+#SBATCH -J  l_test # Add a job name for the array
 #SBATCH -p # Add a partition name
 #SBATCH -c 1 # number of cores
 #SBATCH -t 1-00:00  # Running time in the format - D-HH:MM
 #SBATCH --mem 4000 # Memory request - 1000 corresponds to 1GB
 #SBATCH -o out_%a.out # Standard output
 #SBATCH -e err_%a.err # Standard error
-Rscript # Add the file you want to run here
+Rscript ~/reproducibility/single_test.R # Add the file you want to run here
