f30e2d9467d007fec6e4f8fbab3d4857dee73d65
Souhardya Sengupta <62637393+SSouhardya@users.noreply.github.com>
2024-06-28 05:13:55 -0400

    Create run_ci.sh

---
+++ reproducibility/scripts/run_ci.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -J  l_test # Add a job name for the array
+#SBATCH -p  # Add a partition name
+#SBATCH -c 1 # number of cores
+#SBATCH -t 1-00:00  # Running time in the format - D-HH:MM
+#SBATCH --mem 8000 # Memory request - 1000 corresponds to 1GB
+#SBATCH -o out_%a.out # Standard output
+#SBATCH -e err_%a.err # Standard error
+Rscript ~/reproducibility/ci_simulations.R $1
