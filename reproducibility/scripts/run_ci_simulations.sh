05fbd99b5c7441d2f15fea093ac5bec6b2df0012
Souhardya Sengupta <62637393+SSouhardya@users.noreply.github.com>
2024-06-28 11:13:19 -0400

    Update run_ci_simulations.sh

--- reproducibility/scripts/run_ci_simulations.sh
+++ reproducibility/scripts/run_ci_simulations.sh
@@ -3,4 +3,4 @@ Rscript ~/reproducibility/combine_CI_results.R par_adj_ci_amplitude.RData,1000,1
 Rscript ~/reproducibility/combine_CI_results.R par_adj_ci_amplitude_known_sigma.RData,1000,1
 Rscript ~/reproducibility/combine_CI_results.R par_unadj_ci_amplitude.RData,50,1
 Rscript ~/reproducibility/combine_CI_results.R par_unadj_ci_correlation.RData,50,0,correlation
-Rscript ~/reproducibility/combine_CI_results.R par_unadj_ci_sparsity.RData,50,0,sparsity
+Rscript ~/reproducibility/combine_CI_results.R par_unadj_ci_sparsity.RData,500,0,sparsity
